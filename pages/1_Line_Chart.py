e5ee20a42307b0af11817ca8dc4a3b5155ada41d
Vanh <160717040+vanhhoang1812@users.noreply.github.com>
2024-11-11 23:27:40 +0700

    Update 1_Line_Chart.py

--- pages/1_Line_Chart.py
+++ pages/1_Line_Chart.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #ignore warning
 import datetime as dt
+
 
 st.title('Line Chart')
 
